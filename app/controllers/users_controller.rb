26c5db90eeb0191132135a89c33453a13ccfca6c
Gianfranco Anzalone <gianfranco@anzalone.it>
2014-04-26 07:33:13 -0600

    fine signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
+
 class UsersController < ApplicationController
+	
+def destroy
+  sign_out
+  redirect_to root_url
+  end
+
 	def show
  		@user = User.find(params[:id])
 	end
@@ -8,10 +15,22 @@ class UsersController < ApplicationController
    def create
     @user = User.new(user_params)
     if @user.save
+    	sign_in @user
        flash[:success] = "Welcome to the Sample App!" 	
       redirect_to @user
     else
       render 'new'
+    end
+  end
+  def edit
+  @user=User.find params[:id]
+  end
+def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
     end
   end
 
